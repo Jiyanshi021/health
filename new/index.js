b704299a5fedacb4bc078cf3c38686790b015f58
Jiyanshi021 <122365516+Jiyanshi021@users.noreply.github.com>
2024-04-23 13:31:12 +0530

    Search health Problems

---
+++ new/index.js
@@ -0,0 +1,21 @@
+
+
+function ButtonFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function FilterFunction(){
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.ariaValueMax.toUpperCase();
+    div= document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+}
